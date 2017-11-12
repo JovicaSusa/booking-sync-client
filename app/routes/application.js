0c77a9a196a50727b21cf824a54ee6c03490fc47
Jovica Susa <jovicasusa1985@gmail.com>
2017-11-12 13:30:51 +0100

    Enabled logout

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+const { inject: { service }, Route } = Ember;
+
+export default Route.extend(ApplicationRouteMixin, {
+  session: service(),
+
+  setupController(controller, model) {
+    this._super(...arguments);
+
+    controller.set('session', this.get('session'));
+  },
+
+  actions: {
+    logout() {
+      this.get('session').invalidate().
+        then(() => this.transitionTo('index'));
+    }
+  }
+});
