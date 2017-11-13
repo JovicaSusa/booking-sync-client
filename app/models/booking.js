ea3a392d9e1d1f5ee0de26294f4a82fc0ee5845c
Jovica Susa <jovicasusa1985@gmail.com>
2017-11-13 10:28:36 +0100

    Added booking model

---
+++ app/models/booking.js
@@ -0,0 +1,12 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+
+export default Model.extend({
+  clientEmail: attr('string'),
+  startAt: attr('date'),
+  endAt: attr('date'),
+  price: attr('date'),
+
+  rental: belongsTo()
+});
