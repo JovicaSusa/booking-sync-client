2137a5234255e0a53810145025994809e298480b
Jovica Susa <jovicasusa1985@gmail.com>
2017-11-14 01:56:51 +0100

    Added authenticated route mixin; Fixed tests

--- app/routes/rentals/rental/book.js
+++ app/routes/rentals/rental/book.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model() {
     const rental = this.modelFor('rentals.rental');
     return this.store.createRecord('booking', {
