90e77dde0c8b732082400c240f10ac624773ccca
Jovica Susa <jovicasusa1985@gmail.com>
2017-11-14 13:15:52 +0100

    Fixed validate booking overlap

--- app/validators/validate-overlap.js
+++ app/validators/validate-overlap.js
@@ -1,8 +1,31 @@
+import Ember from 'ember';
+
 export default function validateOverlap() {
   return (key, newValue, oldValue, changes, content) => {
-    const possibleOverlap = content.get('rental.bookings').
-      filter( booking => !booking.get('isNew')).
-      any( booking => newValue.isBetween(booking.get('startAt'), booking.get('endAt')));
+    const changesArray = Object.keys(changes);
+    const bookingsWithPossibleOverlap = Ember.A(content.get('rental.bookings').
+      filter( booking => !booking.get('isNew')));
+    let possibleOverlap = false;
+
+    if (changesArray.includes('startAt') && key === 'endAt') {
+      possibleOverlap = bookingsWithPossibleOverlap.
+        any( booking => {
+          return changes.startAt > booking.get('startAt') &&
+            changes.startAt < booking.get('endAt') ||
+            booking.get('startAt') > changes.startAt &&
+            booking.get('startAt') < newValue;
+        });
+    } else if (changesArray.includes('endAt') && key === 'startAt') {
+      possibleOverlap = bookingsWithPossibleOverlap.
+        any( booking => {
+          return newValue > booking.get('startAt') &&
+            newValue < booking.get('endAt') ||
+            booking.get('startAt') > newValue &&
+            booking.get('startAt') < changes.endAt;
+        });
+    } else {
+      return true;
+    }
 
     if(possibleOverlap) {
       return 'rental is already booked on this dates';
