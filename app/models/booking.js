006cea9ccbd0e5fe56c86cfb889cef708842eba2
Jovica Susa <jovicasusa1985@gmail.com>
2017-11-14 01:37:55 +0100

    Added computed property for period; admin layout changed

--- app/models/booking.js
+++ app/models/booking.js
@@ -1,6 +1,9 @@
+import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
+
+const { computed } = Ember;
 
 export default Model.extend({
   clientEmail: attr('string'),
@@ -8,5 +11,11 @@ export default Model.extend({
   endAt:       attr('utc'),
   price:       attr('number'),
 
-  rental: belongsTo()
+  rental: belongsTo(),
+
+  period: computed('startAt', 'endAt', function() {
+    const { endAt, startAt } = this.getProperties('startAt', 'endAt');
+
+    return endAt.diff(startAt, 'days');
+  })
 });
