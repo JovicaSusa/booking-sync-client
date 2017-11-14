344484106518863fd4fea70be1ae3e9d124d95ef
Jovica Susa <jovicasusa1985@gmail.com>
2017-11-14 15:02:14 +0100

    Fixed component name in templates

--- tests/integration/components/booking-form-test.js
+++ tests/integration/components/booking-form-test.js
@@ -1,6 +1,6 @@
 import { moduleForComponent/*, test */ } from 'ember-qunit';
 // import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('create-booking', 'Integration | Component | create booking', {
+moduleForComponent('booking-form', 'Integration | Component | create booking', {
   integration: true
 });
