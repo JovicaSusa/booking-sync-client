3f729980037f4c92fea877b0f81786259804b9a3
Jovica Susa <jovicasusa1985@gmail.com>
2017-11-14 01:35:32 +0100

    Fixed booking length validator

--- app/validators/booking-length.js
+++ app/validators/booking-length.js
@@ -1,17 +1,17 @@
-export default function validateBookingLength(/*options ={} */) {
+export default function validateBookingPeriod(/*options ={} */) {
   return (key, newValue, oldValue, changes/*, content */) => {
     const changesArray = Object.keys(changes);
-    let bookingLength = null;
+    let bookingPeriod = null;
 
-    if (changesArray.includes('startAt')) {
-      bookingLength = newValue.diff(changes.startAt, 'days')
-    } else if (changesArray.includes('endAt')) {
-      bookingLength = changes.endAt.diff(newValue, 'days');
+    if (changesArray.includes('startAt') && key === 'endAt') {
+      bookingPeriod = newValue.diff(changes.startAt, 'days')
+    } else if (changesArray.includes('endAt') && key === 'startAt') {
+      bookingPeriod = changes.endAt.diff(newValue, 'days');
     } else {
       return true;
     }
 
-    if (bookingLength >= 1) {
+    if (bookingPeriod >= 1) {
       return true;
     } else {
       return 'booking period must be at least one night/day'
